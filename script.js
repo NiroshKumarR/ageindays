f955c365f657a39351ccd10496dc58df3aacec3a
Nirosh kumar.R <niroshkumar072@gmail.com>
2020-09-26 08:57:40 +0000

    small cahnge

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function ageindays(){
     var birthyaer = prompt('Which year you were born?');
     var agedays = (2020 - birthyaer)*365;
     var h1= document.createElement('h1');
-    var answer = document.createTextNode('I think your are '+ agedays +' days alive probably still alive to check this with me.');
+    var answer = document.createTextNode('I think your are '+ agedays +' days alive, and probably still alive to check this with me.');
     h1.setAttribute('id','results');
     h1.appendChild(answer);
     document.getElementById('results').appendChild(h1);
